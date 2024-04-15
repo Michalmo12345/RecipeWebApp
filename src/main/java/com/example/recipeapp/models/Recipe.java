f733cd2f4eef420883a14350a0d14fa383e138e5
Jasiek <jan.szymczak10@gmail.com>
2024-04-15 22:48:16 +0200

    Fix folder differences

--- src/main/java/com/example/recipeapp/models/Recipe.java
+++ src/main/java/com/example/recipeapp/models/Recipe.java
@@ -1,8 +1,10 @@
 package com.example.recipeapp.models;
 
+import com.example.recipeapp.run.Ingredient;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "recipes")
@@ -13,13 +15,66 @@ public class Recipe {
     public Integer id;
     private String name;
     private String image;
-    private String category;
+    private List<String> category;
     private String instructions;
-    private Integer time;
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Ingredient> ingredients;
+    private double time;
+    private List<com.example.recipeapp.run.Ingredient> ingredients;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id",nullable = false)
-    private User user;
+    public Recipe() {
+    }
+
+
+    public Recipe(Integer id, String image, String name, List<String> category, String instructions, double time, List<com.example.recipeapp.run.Ingredient> ingredients) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+        this.category = category;
+        this.instructions = instructions;
+        this.time = time;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCategory() {
+        return category;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public List<com.example.recipeapp.run.Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id + "\n" +
+                ", image='" + image + '\'' + "\n" +
+                ", name='" + name + '\'' + "\n" +
+                ", category=" + category + "\n" +
+                ", instructions='" + instructions + '\'' + "\n" +
+                ", ingredients=" + ingredients.stream().map(Ingredient::toString).collect(Collectors.joining(", ")) + "\n" +
+                ", time=" + time + "\n" +
+                '}';
+    }
 }
