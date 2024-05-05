19f0061d39bb0f06833956a9a6d7939f159f721c
mmokrzyc <01178234@pw.edu.pl>
2024-05-05 18:11:28 +0200

    not  working api handling

--- src/main/java/com/example/recipeapp/RecipeAppApplication.java
+++ src/main/java/com/example/recipeapp/RecipeAppApplication.java
@@ -3,6 +3,7 @@ package com.example.recipeapp;
 import com.example.recipeapp.models.Ingredient;
 import com.example.recipeapp.models.Recipe;
 
+import com.example.recipeapp.repositories.RecipeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,22 +15,17 @@ import java.util.List;
 public class RecipeAppApplication {
 
     private static final Logger log = LoggerFactory.getLogger(RecipeAppApplication.class);
+
     public static void main(String[] args) {
 
         SpringApplication.run(RecipeAppApplication.class, args);
     }
 
 //    @Bean
-//    CommandLineRunner runner(RecipeRepository recipeRepository) {
+//    CommandLineRunner reciper(RecipeRepository recipeRepository) {
 //        return args -> {
-//            Recipe recipe = new Recipe(2,"Spaghetti", "io", List.of("Italian", "Pasta"), "inn",123, List.of(new Ingredient("Pasta", "200g", 2), new Ingredient("Tomato Sauce", "500g", 1)));
-//            log.info(recipe.toString());
-////            recipeRepository.drop();
-//       //     recipeRepository.create(recipe);
-//            List<Recipe> x = recipeRepository.findAll();
-//            for (Recipe r : x) {
-//                log.info(r.toString());
-//            }
+//            Recipe recipe = new Re
 //        };
 //    }
+
 }
--- src/main/java/com/example/recipeapp/controlers/RecipeController.java
+++ src/main/java/com/example/recipeapp/controlers/RecipeController.java
@@ -1,60 +1,47 @@
 package com.example.recipeapp.controlers;
 
 
+import com.example.recipeapp.api.RecipeApiHandler;
 import com.example.recipeapp.models.Recipe;
 import com.example.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class RecipeController {
     private RecipeService recipeService;
+    private RecipeApiHandler recipeApiHandler;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-//    private final RecipeRepository recipeRepository;
-//
-//    public RecipeController(RecipeRepository recipeRepository) {
-//        this.recipeRepository = recipeRepository;
-//    }
-//
-//    @GetMapping("/recipe/{id}")
-//    Recipe findById(@PathVariable Integer id){
-//        Optional<Recipe> recipe = recipeRepository.findById(id);
-//        if(recipe.isEmpty())
-//        {
-//            throw new RecipeNotFoundException();
-//        }
-//        return recipe.get();
-//    }
-//
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("message", "Witaj na stronie gotuj z KLASĄ");
         return "index";
     }
-//
-    @GetMapping("/add-recipe")
-    public String addRecipe(Model model) {
-        model.addAttribute("recipe", new Recipe());
-        return "add-recipe";
-    }
-//
-//    @GetMapping("api/recipes")
-//    @ResponseBody
-//    List<Recipe> findAll() {
-//        return recipeRepository.findAll();
-//    }
+
+
     @GetMapping("/view-recipes")
     public String viewRecipes(Model model) {
         return "view-recipes";
     }
-//
-//    @GetMapping("/search-recipes")
-//    public String showSearchForm() {
-//        return "search-recipes"; // Return the search-recipes.html template
-//    }
+
+    @GetMapping("/search-recipes")
+    public String searchRecipes(@RequestParam(required = false) String query, Model model) {
+        List<Recipe> recipes;
+        if (query != null && !query.isEmpty()) {
+            recipes = recipeApiHandler.getRecipes(query);
+        } else {
+            recipes = List.of(); // or provide some default list of recipes
+        }
+        model.addAttribute("recipes", recipes);
+        return "api-recipes"; // This is the name of the Thymeleaf template (e.g., recipes.html under src/main/resources/templates)
+    }
 
 }
